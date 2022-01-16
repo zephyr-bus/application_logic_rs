d3bbf7ef79c0b3a5a7edf1789132a3f4b08bcdf1
Rodrigo Peixoto <rodrigopex@edge.ufal.br>
2022-01-16 19:19:53 -0300

    Add macros to simplify unsafe C functions call
    Signed-off-by: Rodrigo Peixoto <rodrigopex@edge.ufal.br>

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ all: clean header_gen lib build_sample run
 lib:
 	cargo build --package application_logic_rs --target thumbv7em-none-eabi --release --no-default-features
 
+expand:
+	cargo +nightly expand --package application_logic_rs --target thumbv7em-none-eabi --release --no-default-features
+
 header_gen:
 	cbindgen --config cbindgen.toml --crate application_logic_rs --output generated/application_logic_rs.h --lang C
 
--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,27 @@ extern "C" {
     pub fn printk(format: *const u8, ...);
     pub fn rs_msleep(ms: i32);
 }
+macro_rules! k_msleep_unsafe {
+    ($e:expr) => {
+        unsafe {
+            rs_msleep($e);
+        }
+    };
+}
+
+macro_rules! printk_unsafe {
+	($string:literal) => {
+		unsafe {
+			printk($string.as_ptr());
+		}
+	};
+	($format:expr, $($e:expr),+) => {
+		unsafe {
+			printk($format.as_ptr(), $($e),+);
+		}
+
+	};
+}
 
 #[no_mangle]
 pub extern "C" fn smean_rs(scaling: i32, ptr: *const i32, size: usize) -> i32 {
@@ -34,15 +55,11 @@ pub fn smean(scaling: i32, array: &[i32]) -> i32 {
 
 #[no_mangle]
 pub extern "C" fn application_logic() {
-    unsafe {
-        printk("Hello World from rust!\n".as_ptr());
-    }
+    printk_unsafe!("Hello World from rust!\n");
     let t = [2, 2, 2, 2, 2];
     loop {
         let mean = smean(1, &t);
-        unsafe {
-            printk("smean = %d\n".as_ptr(), mean);
-            rs_msleep(1000);
-        }
+        printk_unsafe!("smean = %d\n", mean);
+        k_msleep_unsafe!(1000);
     }
 }
